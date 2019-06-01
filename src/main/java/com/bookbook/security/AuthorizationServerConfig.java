2fab19edaa66c11a9066ff03060568afec201b2e
aploskiy <ploskiy.andriy@gmail.com>
2019-06-01 18:45:41 +0300

    Implement oauth2

---
+++ src/main/java/com/bookbook/security/AuthorizationServerConfig.java
@@ -0,0 +1,10 @@
+package com.bookbook.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+}
