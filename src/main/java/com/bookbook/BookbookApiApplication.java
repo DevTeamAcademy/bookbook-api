948f1cadb14d550984a67fbcacb79ec636e9a005
aploskiy <ploskiy.andriy@gmail.com>
2019-06-01 13:54:02 +0300

    Implement oauth2

---
+++ src/main/java/com/bookbook/BookbookApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbookApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookbookApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bookbook/BookbookApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookbookApiApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
