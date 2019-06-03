4fe0e83c613d64c1cc1b9a8348d8082d09761d6e
aploskiy <ploskiy.andriy@gmail.com>
2019-06-04 00:11:58 +0300

    Implement oauth2

--- src/main/java/com/bookbook/BookbookApiApplication.java
+++ src/main/java/com/bookbook/BookbookApiApplication.java
@@ -2,22 +2,12 @@ package com.bookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableAuthorizationServer
-//@EnableResourceServer
-@RestController
 public class BookbookApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BookbookApiApplication.class, args);
-  }
-
-  @RequestMapping(value = "/products")
-  public String getProductName() {
-    return "Honey";
   }
 
 }
