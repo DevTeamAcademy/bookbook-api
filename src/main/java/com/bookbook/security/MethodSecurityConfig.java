c9ab4aaaaef5f9c2c43ced582a2554aaead29a64
aploskiy <ploskiy.andriy@gmail.com>
2019-06-09 14:54:18 +0300

    Implement BookBook

--- src/main/java/com/bookbook/security/MethodSecurityConfig.java
+++ src/main/java/com/bookbook/security/MethodSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.bookbook.general.config;
+package com.bookbook.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
